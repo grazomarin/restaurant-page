0a4d58ebaf0d2fff3d2fe66c194a72e356158068
grazomarin <huseynovk355@gmail.com>
2022-09-02 17:29:02 +0400

    initiation

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './style.css';
+
+const container = document.getElementById('container')
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const webpack = require('webpack');
+
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(woff2|woff|otf)$/,
+                use: 'asset/resource'
+            }
+        ]
+    },
+    devServer: {
+        static: './dist',
+        hot: true,
+    },
+    output: {
+        path: path.dirname(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+/*     optimization: {runtimeChunk: 'single',},*/
+}
